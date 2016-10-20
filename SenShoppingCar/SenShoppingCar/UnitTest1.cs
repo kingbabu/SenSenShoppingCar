233a922a8fccc8938872b83dea4ee7820259509f
Ray <faultlesswoman@gmail.com>
2016-10-20 21:58:23 +0800

    add one book test

---
+++ SenShoppingCar/SenShoppingCar/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SenShoppingCar
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Add_1_Book_To_Shoppingcar_Should_Be_100()
+        {
+            
+
+            Assert.Fail();
+        }
+    }
+}
